7e81924455565afd72e51b18681837676f1f7c5b
PolatDeu <ahmetpolat@mein.gmx>
2021-06-24 20:12:58 +0200

    .

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+// let a = 5;
+// console.log(a)
+// {
+//     let a = 8;
+//     console.log(a)
+// }
+// console.log(a)
+
+var a = 5;
+var b = 1;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a**b)
+
+a = --b;
+console.log({a})
+console.log({b})
+console.log(b)
